088389e9157d324c46e51968f10b896f5f5f2e4a
David Ikin <davidikin45@gmail.com>
2020-04-10 10:29:59 +0100

    changes

--- src/DND.Data.Identity/IdentityContextInitializer.cs
+++ src/DND.Data.Identity/IdentityContextInitializer.cs
@@ -4,6 +4,7 @@ using DND.Data.Identity.Initializers;
 using DND.Domain.Identity;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Threading;
@@ -14,35 +15,34 @@ namespace DND.Data.Identity
     public class IdentityContextInitializer : DbStartupTaskBlocking
     {
         private readonly IHostEnvironment _hostingEnvironment;
-        private readonly IdentityContext _context;
         private readonly IPasswordHasher<User> _passwordHasher;
 
         public override int Order => 0;
 
-        public IdentityContextInitializer(IServiceProvider serviceProvider, IdentityContext context, IPasswordHasher<User> passwordHasher, IHostEnvironment hostingEnvironment)
+        public IdentityContextInitializer(IServiceProvider serviceProvider, IPasswordHasher<User> passwordHasher, IHostEnvironment hostingEnvironment)
             :base(serviceProvider)
         {
-            _context = context;
             _passwordHasher = passwordHasher;
             _hostingEnvironment = hostingEnvironment;
         }
 
         protected override async Task ExecuteAsync(IServiceProvider scopedServiceProvider, CancellationToken stoppingToken)
         {
+            var context = scopedServiceProvider.GetRequiredService<IdentityContext>();
             if (_hostingEnvironment.IsStaging() || _hostingEnvironment.IsProduction())
             {
                 var migrationInitializer = new IdentityContextInitializerMigrate(_passwordHasher);
-                await migrationInitializer.InitializeAsync(_context);
+                await migrationInitializer.InitializeAsync(context);
             }
             else if (_hostingEnvironment.IsIntegration())
             {
                 var migrationInitializer = new IdentityContextInitializerDropMigrate(_passwordHasher);
-                await migrationInitializer.InitializeAsync(_context);
+                await migrationInitializer.InitializeAsync(context);
             }
             else
             {
                 var migrationInitializer = new IdentityContextInitializerDropMigrate(_passwordHasher);
-                await migrationInitializer.InitializeAsync(_context);
+                await migrationInitializer.InitializeAsync(context);
             }
         }
     }
