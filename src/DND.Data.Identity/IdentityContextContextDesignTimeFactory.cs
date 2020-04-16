de7ead4bb85ed4593cf103d8a736c5f9c89aee61
David Ikin <davidikin45@gmail.com>
2020-04-16 17:51:11 +0100

    changes

--- src/DND.Data.Identity/IdentityContextContextDesignTimeFactory.cs
+++ src/DND.Data.Identity/IdentityContextContextDesignTimeFactory.cs
@@ -1,26 +1,37 @@
-﻿using AspNetCore.Mvc.Extensions.Data;
-using Autofac.AspNetCore.Extensions;
+﻿using Autofac.AspNetCore.Extensions;
 using Autofac.AspNetCore.Extensions.Data;
 using Autofac.AspNetCore.Extensions.Data.IdentificationStrategies;
+using EntityFrameworkCore.Initialization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System.Reflection;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace DND.Data.Identity
 {
-    public class IdentityContextContextDesignTimeFactory : DesignTimeDbContextFactoryBase<IdentityContext>
+    //https://medium.com/oppr/net-core-using-entity-framework-core-in-a-separate-project-e8636f9dc9e5
+    public class IdentityContextContextDesignTimeFactory : IDesignTimeDbContextFactory<IdentityContext>
     {
-        public IdentityContextContextDesignTimeFactory()
-            : base("IdentityConnection", typeof(IdentityContext).GetTypeInfo().Assembly.GetName().Name)
+        public IdentityContext CreateDbContext(string[] args)
         {
-        }
+            IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(@Directory.GetCurrentDirectory() + "/../DND.Web/appsettings.json")
+                .AddJsonFile(@Directory.GetCurrentDirectory() + "/../DND.Web/appsettings.Development.json", optional: true).Build();
 
-        protected override IdentityContext CreateNewInstance(IConfiguration configuration, DbContextOptions<IdentityContext> options)
-        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            Console.WriteLine("AppContextDesignTimeFactory.Create(string): Connection string: {0}", connectionString);
+
+            var optionsBuilder = new DbContextOptionsBuilder<IdentityContext>();
+            optionsBuilder.SetConnectionString<IdentityContext>(connectionString);
+
             var services = new ServiceCollection();
-            services.AddSingleton<IDbContextTenantStrategy<IdentityContext>>(sp => new DifferentConnectionFilterTenantDifferentSchemaDbContext<IdentityContext>(this.ConnectionStringName));
-            return new IdentityContext(options, new MultiTenantService(new MultiTenantDbContextStrategyService(services.BuildServiceProvider()), new Tenant(null, configuration)));
+            services.AddSingleton<IDbContextTenantStrategy<IdentityContext>>(sp => new DifferentConnectionFilterTenantDifferentSchemaDbContext<IdentityContext>("DefaultConnection"));
+
+            return new IdentityContext(optionsBuilder.Options, new MultiTenantService(new MultiTenantDbContextStrategyService(services.BuildServiceProvider()), new Tenant(null, configuration)));
         }
     }
 }
--- src/DND.Data/AppContextDesignTimeFactory.cs
+++ src/DND.Data/AppContextDesignTimeFactory.cs
@@ -2,27 +2,38 @@
 using Autofac.AspNetCore.Extensions;
 using Autofac.AspNetCore.Extensions.Data;
 using Autofac.AspNetCore.Extensions.Data.IdentificationStrategies;
+using EntityFrameworkCore.Initialization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Reflection;
 
 namespace DND.Data
 {
     //https://medium.com/oppr/net-core-using-entity-framework-core-in-a-separate-project-e8636f9dc9e5
-    public class AppContextDesignTimeFactory : DesignTimeDbContextFactoryBase<AppContext>
+    public class AppContextDesignTimeFactory : IDesignTimeDbContextFactory<AppContext>
     {
-        public AppContextDesignTimeFactory()
-            : base("DefaultConnection", typeof(AppContext).GetTypeInfo().Assembly.GetName().Name)
+        public AppContext CreateDbContext(string[] args)
         {
+            IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile(@Directory.GetCurrentDirectory() + "/../DND.Web/appsettings.json")
+               .AddJsonFile(@Directory.GetCurrentDirectory() + "/../DND.Web/appsettings.Development.json", optional: true).Build();
 
-        }
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
 
-        protected override AppContext CreateNewInstance(IConfiguration configuration, DbContextOptions<AppContext> options)
-        {
+            Console.WriteLine("AppContextDesignTimeFactory.Create(string): Connection string: {0}", connectionString);
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppContext>();
+            optionsBuilder.SetConnectionString<AppContext>(connectionString);
+
             var services = new ServiceCollection();
-            services.AddSingleton<IDbContextTenantStrategy<AppContext>>(sp => new DifferentConnectionFilterTenantDifferentSchemaDbContext<AppContext>(this.ConnectionStringName));
-            return new AppContext(options, new MultiTenantService(new MultiTenantDbContextStrategyService(services.BuildServiceProvider()), new Tenant(null, configuration)));
+            services.AddSingleton<IDbContextTenantStrategy<AppContext>>(sp => new DifferentConnectionFilterTenantDifferentSchemaDbContext<AppContext>("DefaultConnection"));
+
+            return new AppContext(optionsBuilder.Options, new MultiTenantService(new MultiTenantDbContextStrategyService(services.BuildServiceProvider()), new Tenant(null, configuration)));
         }
     }
 }
